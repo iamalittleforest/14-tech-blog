6344f276fe81176c9ac63edb97db9af9b1ab12c0
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-16 20:25:09 -0700

    add relationship info

--- models/index.js
+++ models/index.js
@@ -4,17 +4,34 @@ const Post = require('./Post');
 const User = require('./User');
 
 // Comment belongsTo User
+Comment.belongsTo(User, {
+
+});
 
 // Comment belongsTo Post
+Comment.belongsTo(Post, {
+
+});
 
 // Post hasMany Comments
+Post.hasMany(Comment, {
+
+});
 
 // Post belongsTo User
+Post.belongsTo(User, {
+
+});
 
 // User hasMany Comments
+User.hasMany(Comment, {
+
+});
 
 // User hasMany Posts
+User.hasMany(Post, {
 
+});
 
 module.exports = {
   Comment,
