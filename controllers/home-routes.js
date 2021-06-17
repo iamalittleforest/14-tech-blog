c1a6ed351f61579ec72d5167cf7b22043cf31fc0
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-17 15:44:40 -0700

    outline structure of homepage routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,13 @@
+// import routes and models
+const router = require('express').Router();
+const { Comment, Post, User } = require('../models');
+
+// home route
+
+// single post route
+
+// dashboard route
+
+// login route
+
+module.exports = router;
