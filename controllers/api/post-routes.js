a370accf0e7bca5769dc19caed26e5ab1a83a278
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-16 22:04:41 -0700

    outline structure of post routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,15 @@
+// import routes and models
+const router = require('express').Router();
+const { Category, Post } = require('../../models');
+
+// GET all posts
+
+// GET single post by ID
+
+// CREATE new post
+
+// UPDATE post by ID
+
+// DELETE post by ID
+
+module.exports = router;
