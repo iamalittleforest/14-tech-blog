fcfa737f9cd29e5c2c34126e391ac31dfcacbdf8
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-17 12:30:35 -0700

    complete POST, login, and logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,15 +1,72 @@
 // import routes and models
 const router = require('express').Router();
-const { Category, User } = require('../../models');
+const { User } = require('../../models');
 
 // GET all users
 
 // GET single user by ID
 
 // CREATE new user
+router.post('/', async(req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // login
+router.post('/login', async(req, res) => {
+  try {
+    const userData = await User.findOne({ 
+      where: { 
+        username: req.body.username 
+      } 
+    });
+
+    if (!userData) {
+      res.status(400).json({ message: 'No username found, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.status(200).json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // logout
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
