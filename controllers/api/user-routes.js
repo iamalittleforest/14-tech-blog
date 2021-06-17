80be54b5cfe08422d401028bd3bb504063ca35b9
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-17 14:06:50 -0700

    complete GET functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,8 +3,35 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // GET all users
+router.get('/', async(req, res) => {
+  try {
+    const userData = await User.findAll();
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // GET single user by ID
+router.get('/', async(req, res) => {
+  try {
+    const userData = await User.findByPk({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // CREATE new user
 router.post('/', async(req, res) => {
