90c1db5785ef2c248d144ec0b3418f2c49724238
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-16 21:13:34 -0700

    complete column definitions

--- models/User.js
+++ models/User.js
@@ -12,13 +12,21 @@ User.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [8],
+      },
     }
   },
   {
