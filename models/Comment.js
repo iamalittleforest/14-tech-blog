f46e89d55ee7713f5a0f6b44d9e100606758c3be
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-16 21:38:40 -0700

    update DataType for comment and content to TEXT

--- models/Comment.js
+++ models/Comment.js
@@ -15,7 +15,7 @@ Comment.init(
       autoIncrement: true
     },
     comment: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false
     },
     user_id: {
--- models/Post.js
+++ models/Post.js
@@ -19,7 +19,7 @@ Post.init(
       allowNull: false,
     },
     content: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     user_id: {
