ea7cf772dc263d69850847eba21f0c4b8f40d0a0
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-16 22:06:25 -0700

    outline structure of comment routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,9 @@
+// import routes and models
+const router = require('express').Router();
+const { Category, Comment } = require('../../models');
+
+// GET all comments
+
+// CREATE new comment
+
+module.exports = router;
