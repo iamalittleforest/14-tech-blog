0777dd7e90cfe83bbdc95518838b3f2870a45aed
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-16 22:10:54 -0700

    outline structure of user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,15 @@
+// import routes and models
+const router = require('express').Router();
+const { Category, User } = require('../../models');
+
+// GET all users
+
+// GET single user by ID
+
+// CREATE new user
+
+// login
+
+// logout
+
+module.exports = router;
