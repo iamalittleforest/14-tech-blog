b4a7d645006135c818fdb734f212a16f73a248c6
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-17 15:55:30 -0700

    complete single post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,6 +26,31 @@ router.get('/', async(req, res) => {
 });
 
 // single post route
+router.get('/post/:id', async(req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment']
+        }
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // dashboard route
 
