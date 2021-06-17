e5b6e89ab899c3c6a3e4e86a0984eb6a080b933e
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-16 20:47:07 -0700

    fix incorrect info and complete column definitions

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 // import database connection from connection.js
 const sequelize = require('../config/connection.js');
 
-// initialize Category model (table) by extending off Sequelize's Model class
+// initialize Comment model (table) by extending off Sequelize's Model class
 class Comment extends Model {}
 
 // set up fields and rules for Comment model
@@ -12,15 +12,29 @@ Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     comment: {
       type: DataTypes.STRING,
+      allowNull: false
     },
     user_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
     },
     post_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
     }
   },
   {
