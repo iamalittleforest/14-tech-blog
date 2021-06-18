01721111a5eb279c46ef29605f6b4e2092f450be
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-18 09:27:00 -0700

    outline structure of dashboard routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,11 @@
+// import routes and models
+const router = require('express').Router();
+const { Comment, Post, User } = require('../models');
+
+// dashboard route
+
+// new post route
+
+// edit post route
+
+module.exports = router;
