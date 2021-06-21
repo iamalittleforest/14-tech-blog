68ecdffcc57e531af8596f4579eca2315ccb98a3
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-20 23:38:14 -0700

    complete functionality of signupHandler function and add comments

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -2,9 +2,24 @@
 const signupHandler = async (event) => {
   event.preventDefault();
 
-  // identify inputs
+  // collect inputs
   const name = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    // redirect to dashboard if sign up is successful
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
 
 // listen for submit
